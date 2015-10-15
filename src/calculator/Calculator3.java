761ef882f138f7cf12df42968a7d7c04eaceda98
John W <someguy555@gmail.com>
2015-10-15 17:33:14 +0200

    Removed Focus. Has strange calculation bugs!

--- src/calculator/Calculator3.java
+++ src/calculator/Calculator3.java
@@ -5,17 +5,12 @@
  */
 package calculator;
 
-import java.lang.reflect.Method;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
@@ -27,6 +22,10 @@ public class Calculator3 extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        
+        TextField tf1 = new TextField();
+        tf1.requestFocus();
+        
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
         Parent root = loader.load();
         FXMLDocumentController controller = loader.getController();
--- src/calculator/FXMLDocumentController.java
+++ src/calculator/FXMLDocumentController.java
@@ -22,8 +22,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
